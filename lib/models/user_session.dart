e2fb42ad1068f4bc7fa2c373c9f230d7fd7f085e
jj2644348-eng <jj2644348@gmail.com>
2026-08-31 01:38:55 +0530

    Create user_session.dart

---
+++ lib/models/user_session.dart
@@ -0,0 +1,8 @@
+class CurrentUser {
+  static String id = "0001";
+  static String name = "Love Party Owner";
+  static String avatar = "👑";
+  static int coins = 100000;
+  static bool isOwner = true;
+}
+
