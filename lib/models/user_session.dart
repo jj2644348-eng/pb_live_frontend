215757c5a49d46c65d7cfad6567c68eb18560756
jj2644348-eng <jj2644348@gmail.com>
2026-09-01 00:05:41 +0530

    Delete lib directory

--- lib/models/user_session.dart
+++
@@ -1,8 +0,0 @@
-class CurrentUser {
-  static String id = "0001";
-  static String name = "Love Party Owner";
-  static String avatar = "👑";
-  static int coins = 100000;
-  static bool isOwner = true;
-}
-
